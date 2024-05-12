38bcadcc21adff47443c0e6e970e54d9ae2c804b
Joao-Schio <jp@santoandrea.com.br>
2024-05-12 18:57:11 -0400

    adicionado script de teste

--- testar.sh
+++ testar.sh
@@ -4,9 +4,21 @@ set -e
 # define an int var
 declare -i i=0
 g++ src/*.cpp -o pccm -Wall -Werror
+
+for variable in {0..4}
+do
+  ./pccm g-5-6.txt "$variable" > out.txt
+  diff out.txt "g-5-6-$variable.out" -w
+done
+
+echo "Teste 5-6 passou"
+
+
 for variable in {0..9}
 do
   ./pccm g-10-30.txt "$variable" > out.txt
   diff out.txt "g-10-30-$variable.out" -w
-  echo "Test $variable passed"
 done
+
+echo "Teste 10-30 passou"
+
