d62ee4d115da82b91326bef07e889af95ea33769
Joao Pedro Ortega <jp@santoandrea.com.br>
2024-06-21 18:12:10 -0400

    nao precisa de openmp

--- testar.sh
+++ testar.sh
@@ -3,76 +3,68 @@ set -e
 
 # define an int var
 declare -i i=0
-g++ src/*.cpp -o pccm -Wall -Werror -std=c++20 -fopenmp
+g++ src/*.cpp -o pccm -Wall -Werror -std=c++20
 
-for variable in {0..4}
-do
-  ./pccm g-5-6.txt "$variable" > out.txt
-  diff out.txt "g-5-6-$variable.out" -w
+for variable in {0..4}; do
+	./pccm g-5-6.txt "$variable" >out.txt
+	diff out.txt "g-5-6-$variable.out" -w
 done
 
 echo "Teste 5-6 passou"
 
-
-for variable in {0..9}
-do
-  ./pccm g-10-30.txt "$variable" > out.txt
-  diff out.txt "g-10-30-$variable.out" -w
+for variable in {0..9}; do
+	./pccm g-10-30.txt "$variable" >out.txt
+	diff out.txt "g-10-30-$variable.out" -w
 done
 
 echo "Teste 10-30 passou"
 
-
-for variable in {0..4}
-do
-  ./pccm g-neg-c-5-6.txt "$variable" > out.txt
-  diff out.txt "g-neg-c-5-6-$variable.out" -w -I "C"
+for variable in {0..4}; do
+	./pccm g-neg-c-5-6.txt "$variable" >out.txt
+	diff out.txt "g-neg-c-5-6-$variable.out" -w -I "C"
 done
 
 echo "Teste c-neg-5-6 passou"
 
-for variable in {0..9}
-do
-  ./pccm g-neg-c-10-30.txt "$variable" > out.txt
-  diff out.txt "g-neg-c-10-30-$variable.out" -w -I "C" -I "E"
+for variable in {0..9}; do
+	./pccm g-neg-c-10-30.txt "$variable" >out.txt
+	diff out.txt "g-neg-c-10-30-$variable.out" -w -I "C" -I "E"
 done
 
 echo "Teste c-neg-10-30 passou"
 
-./pccm g-100-300.txt 12 > out.txt
+./pccm g-100-300.txt 12 >out.txt
 diff out.txt "g-100-300-12.out" -w
-./pccm g-100-300.txt 37 > out.txt
+./pccm g-100-300.txt 37 >out.txt
 diff out.txt "g-100-300-37.out" -w
-./pccm g-100-300.txt 84 > out.txt
+./pccm g-100-300.txt 84 >out.txt
 diff out.txt "g-100-300-84.out" -w
 echo "g-100-300 passou"
-./pccm g-100-1000.txt 12 > out.txt
+./pccm g-100-1000.txt 12 >out.txt
 diff out.txt "g-100-1000-12.out" -w
 echo "teste  100 1000 12 passou"
-./pccm g-100-1000.txt 37 > out.txt
+./pccm g-100-1000.txt 37 >out.txt
 diff out.txt "g-100-1000-37.out" -w
 echo "teste  100 1000 37 passou"
-./pccm g-100-1000.txt 84 > out.txt
+./pccm g-100-1000.txt 84 >out.txt
 diff out.txt "g-100-1000-84.out" -w
 echo "g-100-1000 passou"
 echo "g 1000-10000 comecando"
-./pccm g-1000-10000.txt 31 > out.txt
+./pccm g-1000-10000.txt 31 >out.txt
 diff out.txt "g-1000-10000-31.out" -w
 echo "g-1000-10000 31 passou"
-./pccm g-1000-10000.txt 265 > out.txt
+./pccm g-1000-10000.txt 265 >out.txt
 diff out.txt "g-1000-10000-265.out" -w
 echo "g-1000-10000 265 passou"
-./pccm g-1000-10000.txt 403 > out.txt
+./pccm g-1000-10000.txt 403 >out.txt
 diff out.txt "g-1000-10000-403.out" -w
 echo "g-1000-10000 403 passou"
-./pccm g-1000-10000.txt 697 > out.txt
+./pccm g-1000-10000.txt 697 >out.txt
 diff out.txt "g-1000-10000-697.out" -w
 echo "g-1000-10000 697 passou"
-./pccm g-1000-10000.txt 812 > out.txt
+./pccm g-1000-10000.txt 812 >out.txt
 diff out.txt "g-1000-10000-812.out" -w
 echo "g-1000-10000 812 passou"
-./pccm g-1000-10000.txt 963 > out.txt
+./pccm g-1000-10000.txt 963 >out.txt
 diff out.txt "g-1000-10000-963.out" -w
 echo "g-1000-10000 passou"
-
-
